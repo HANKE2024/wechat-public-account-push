994e1266b70f250a835f90e28bbd6cabea445c5d
HANKE2024 <499109492@qq.com>
2024-06-15 18:07:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝萍',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oGY7i6iQ2e4yQSycoJz6axsWdE9o',
+      id: 'oGY7i6iQ2e4yQSycoJz6axsWdE9o','oGY7i6rtVhzg5siOppKERJrHNrMQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'HQBhPmhYdv7-JgbEX5SNugqVJVzqmpEm15KUPLKcBAk',
+      useTemplateId: 'iuKVZeX3oMlo76LBCtT1kG-sZ5WhCpAi3X0vKCCOV0Y',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-23',
       festivals: [
