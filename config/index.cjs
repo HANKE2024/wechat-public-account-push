17c6de55708ee34ca62eef5028994f5aad7c0612
HANKE2024 <499109492@qq.com>
2024-06-15 15:54:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '惠萍。',
+      name: '老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGY7i6iQ2e4yQSycoJz6axsWdE9o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '4cJY6_dJ-eKzrPuDQHxisioWyHl7tj3XOrd58WeSHXY',
+      useTemplateId: 'Jrt_ePTYT8gIGHwe6I2rJccd7Gqwd60kdZYWnAyGQ7w',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-23',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '4cJY6_dJ-eKzrPuDQHxisioWyHl7tj3XOrd58WeSHXY',
+  CALLBACK_TEMPLATE_ID: 'Jrt_ePTYT8gIGHwe6I2rJccd7Gqwd60kdZYWnAyGQ7w',
 
   CALLBACK_USERS: [
     {
