10b15b1bcf4596902e20c78d380c2aca11755e38
HANKE2024 <499109492@qq.com>
2024-06-15 15:33:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,24 +19,24 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '惠萍。',
+      name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGY7i6iQ2e4yQSycoJz6axsWdE9o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'd_96DgLrnOp6djq4PCK-_07782mL1_P3Kyp-UT7IRAc',
+      useTemplateId: 'oGY7i6iQ2e4yQSycoJz6axsWdE9o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-23',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝萍', year: '1989', date: '02-16',
+          type: '*生日', name: '宝贝', year: '1989', date: '03-23',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '老公', year: '1989', date: '09-29',
+          type: '生日', name: '李四', year: '1989', date: '09-29',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2023', date: '06-28',
+          type: '节日', name: '相识纪念日', year: '2024', date: '04-30',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -44,14 +44,14 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2024-04-30' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2024-10-06' },
+        { keyword: 'marry_day', date: '2023-06-28' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'oGY7i6iQ2e4yQSycoJz6axsWdE9o',
+  CALLBACK_TEMPLATE_ID: 'lOUIsEhOiTi_3wKgS7PjkvvSntkuDPvE0KjhlD0eLBA',
 
   CALLBACK_USERS: [
     {
