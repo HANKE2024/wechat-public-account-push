f45b6c6a0d8897248669d97d4017f5747fcd918a
HANKE2024 <499109492@qq.com>
2024-06-15 17:45:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,10 +13,6 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'e1a46122d7e502c986915c4bb1a363a0',
 
-  // 是否给文字设置多彩颜色, 和emoji不兼容
-  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  IS_SHOW_COLOR: true,
-
   PROVINCE: '广东',
   CITY: '深圳',
 
@@ -27,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGY7i6iQ2e4yQSycoJz6axsWdE9o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'qJn6mzgSVktcHXSF9hbQU-p-GD6m4s5OgiJDOe3epko',
+      useTemplateId: 'YjwP1-zlEvvmuph3Qtd3fdXGw6nWzfARArmoF54kJAI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-23',
       festivals: [
@@ -37,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '相公', year: '1989', date: '09-29',
+          type: '生日', name: '宝萍', year: '1989', date: '03-23',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '06-28',
