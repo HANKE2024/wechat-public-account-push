ed03f27616bbde66f19c9ebe080769557a813194
HANKE2024 <499109492@qq.com>
2024-06-15 15:14:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGY7i6iQ2e4yQSycoJz6axsWdE9o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Jrt_ePTYT8gIGHwe6I2rJccd7Gqwd60kdZYWnAyGQ7w',
+      useTemplateId: 'pCXjgJ5eM-LBfvTSWym4BPdxQnjgMFVodxZ2cwP7__U	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-23',
       festivals: [
